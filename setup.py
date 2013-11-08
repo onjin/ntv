a1abaa50f59f8cfedbdd5fcbc5a179179ea4f5e4
Marek <onjinx@gmail.com>
2013-11-08 22:11:00 +0100

    Fixed -d option, bumped version 0.3.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='ntv',
-    version='0.3.2',
+    version='0.3.3',
     description='n.tv api',
     long_description=readme + '\n\n' + history,
     author='Marek Wywiał',
