1d8b3cdc9a93adfc771bf2efc24d328f1f9dd219
Marek Wywiał <onjinx@gmail.com>
2016-07-02 21:18:22 +0200

    fix channels movies by channel id `ntv-cli channels -c 525`

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='ntv',
-    version='0.4.1',
+    version='0.4.2',
     description='n.tv api',
     long_description=readme + '\n\n' + history,
     author='Marek Wywiał',
